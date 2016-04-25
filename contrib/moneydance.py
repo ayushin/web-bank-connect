8c94d931a18370947b62941ba1644148323fe4a7
alexis <alexis@ww.net>
2016-04-25 21:44:52 +0300

    New style money dance script uses OS environment and a local configuration file

--- contrib/moneydance.py
+++ contrib/moneydance.py
@@ -6,82 +6,34 @@ Author: Alex Yushin <alexis@ww.net>
 
 """
 
-#
-# 0. Essential system imports
-#
+
+# 0.1 Essential system imports
 import sys
 import os
-from datetime import datetime
 
+# 0.2 Set up logging for debugging...
 import logging
 logging.basicConfig()
 
-#
-#
-# 1. Configuration
-#
-#
-
 # 1.0 We have to have this one hard-coded as MoneyDance does not pass the __file__ to the script
-WBC_HOME="/Users/alexis/Work/Python/web-bank-connect"
-sys.path.append(WBC_HOME)
+WBC_PATH = os.environ['WBC_PATH']
+sys.path.append(WBC_PATH)
 
 # 1.1 Directory where selenium is installed, normally /Library/Python/2.7/site-packages or alike
-SELENIUM_DIR="/Library/Python/2.7/site-packages"
-sys.path.append(SELENIUM_DIR)
+SELENIUM_PATH = os.environ['SELENIUM_PATH']
+sys.path.append(SELENIUM_PATH)
 
-# 1.2 The path to firefox
-FIREFOX_PATH='/Applications/Firefox.app/Contents/MacOS'
-os.environ['PATH'] = FIREFOX_PATH
 
-#
+# 1.2 The path to firefox and phantomjs binary...
+FIREFOX_PATH = '/Applications/Firefox.app/Contents/MacOS'
+# FIREFOX_PATH = os.environ['FIREFOX_PATH']
+os.environ['PATH'] += os.pathsep + FIREFOX_PATH
+
 # 2. Set up local connections...
-#
-from wbc.models import Connection, Account, NEVER
+from local.config import CONNECTIONS
 
-# 1. Configure the connections...
-CONNECTIONS = [
-    Connection(
-        name        = 'BANK1',
-        plugin      = 'nl.bank1',
-        username    = 'user1',
-        password    = 'password1',
-        accounts    = [
-            Account(
-                name            = '12353795871',
-                type            = 'ccard',
-                moneydance_name = 'Visa Card',
-                # It is possible to override the last download date and time, otherwise using Moneydance last
-                # download timestamp
-                last_download = NEVER,
-                # The import will skip inactive account
-                # active = False,
-            ),
-        ]
-    ),
-    Connection(
-        name            = 'BANK2',
-        plugin          = 'nl.bank2',
-        username        = 'user2',
-        password        = 'password2',
-        accounts = [
-            Account(
-                type            = 'current',
-                name            =  'IBAN123',
-                moneydance_name = 'NL BANK IBAN123',
-            ),
-            Account(
-                type            = 'ccard',
-                name            =  '234723984732897982',
-                moneydance_name = 'MasterCard',
-                # last_download   = datetime(2016, 2, 12)
-            ),
-]
-
-#
 # 3. Run the script..
-#
-from wbc.moneydance2015 import download_all_transactions
+from wbc.moneydance import download_all_transactions
 
 download_all_transactions(
     ra = moneydance.getRootAccount(),
